40967b71522c6ca776ea3fd02a155ad8f2226eb6
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-02-05 16:57:25 +0000

    Started creating tests for the API

---
+++ test/HomeAutomationServer.Tests/Controllers/SensorControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using HomeAutomationServer.Controllers;
+using HomeAutomationServer.Data.Entities;
+using HomeAutomationServer.Tests.Mock;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeAutomationServer.Tests.Controllers
+{
+    [TestClass]
+    public class SensorControllerTests
+    {
+        
+        [TestMethod]
+        public async Task PostSensor()
+        {
+            var context = new Context().CreateContext("PostSensor");
+
+            var controller = new SensorController(context);
+
+            var sensor = new SensorEntity { Name = "Light" };
+
+            var response = (CreatedAtActionResult)await controller.PostSensorEntity(sensor);
+
+            Assert.IsTrue(response.StatusCode == 201);
+            Assert.IsTrue(response.ActionName == "GetSensorEntity");
+            Assert.IsTrue(response.RouteValues.Any());
+
+            var responseModel = (SensorEntity)response.Value;
+
+            Assert.IsNotNull(responseModel);
+
+            Assert.IsTrue(responseModel.Id > 0);
+            Assert.IsTrue(responseModel.Name == "Light");
+        }
+        [TestMethod]
+        public async Task GetSensorEntity()
+        {
+            var context = new Context().CreateContext("GetSensorEntity");
+
+            var sensor = new SensorEntity { Name = "Light" };
+
+            context.Add(sensor);
+            await context.SaveChangesAsync();
+
+            var controller = new SensorController(context);
+            var response = (OkObjectResult)await controller.GetSensorEntity(sensor.Id);
+
+            Assert.IsTrue(response.StatusCode == 200);
+
+            var responseModel = (SensorEntity)response.Value;
+
+            Assert.IsNotNull(responseModel);
+
+            Assert.IsTrue(responseModel.Id > 0);
+            Assert.IsTrue(responseModel.Name == "Light");
+        }
+
+        [TestMethod]
+        public async Task GetSensors()
+        {
+            var context = new Context().CreateContext("GetSensors");
+
+            var sensors = new List<SensorEntity>(){ new SensorEntity { Name = "Light" }, new SensorEntity { Name = "Thermal" } };
+
+            context.AddRange(sensors);
+            await context.SaveChangesAsync();
+
+            var controller = new SensorController(context);
+            var responseModel = (IEnumerable<SensorEntity>) controller.GetSensors();
+
+            //Assert.IsTrue(response.StatusCode == 200);
+
+            //var responseModel = (List<SensorEntity>)response.Value;
+
+            Assert.IsNotNull(responseModel);
+
+            Assert.IsTrue(responseModel.Any());
+            Assert.IsNotNull(responseModel.Where(x => x.Name == "Light").FirstOrDefault());
+            Assert.IsNotNull(responseModel.Where(x => x.Name == "Thermal").FirstOrDefault());
+        }
+    }
+}
---
+++ test/HomeAutomationServer.Tests/Mock/Context.cs
@@ -0,0 +1,21 @@
+﻿using HomeAutomationServer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAutomationServer.Tests.Mock
+{
+    public class Context 
+    {
+        public ApplicationDbContext CreateContext(string inMemoryName)
+        {
+            var options = OptionsBuilder(inMemoryName);
+            return new ApplicationDbContext(options.Options);
+        }
+        private DbContextOptionsBuilder<ApplicationDbContext> OptionsBuilder(string inMemoryName)
+        {
+            return new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(inMemoryName);
+        }
+    }
+}
